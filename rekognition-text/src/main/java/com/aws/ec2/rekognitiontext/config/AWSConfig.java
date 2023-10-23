ce10be86f4af8d24c41983d29d01c757e6700819
raghusagar13 <Raghusagar896@gmail.com>
2023-10-24 00:00:08 +0530

    config change

--- rekognition-text/src/main/java/com/aws/ec2/rekognitiontext/config/AWSConfig.java
+++ rekognition-text/src/main/java/com/aws/ec2/rekognitiontext/config/AWSConfig.java
@@ -2,6 +2,7 @@ package com.aws.ec2.rekognitiontext.config;
 
 import com.amazon.sqs.javamessaging.ProviderConfiguration;
 import com.amazon.sqs.javamessaging.SQSConnectionFactory;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.rekognition.AmazonRekognition;
 import com.amazonaws.services.rekognition.AmazonRekognitionClientBuilder;
 import com.amazonaws.services.s3.AmazonS3;
@@ -14,9 +15,11 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AWSConfig {
 
+    Regions clientRegion = Regions.US_EAST_1;
+
     @Bean
     public AmazonS3 amazonS3Client() {
-        return AmazonS3ClientBuilder.defaultClient();
+        return AmazonS3ClientBuilder.standard().withRegion(clientRegion).build();
     }
 
     @Bean
